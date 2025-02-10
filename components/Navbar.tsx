e345e4416938a286ca4eaa5b80501f7ec1280091
headlessripper <167107185+headlessripper@users.noreply.github.com>
2025-02-10 17:05:08 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
           className="max-sm:size-10"
         />
         <p className="text-[26px] font-extrabold text-white max-sm:hidden">
-          BoBaas Academy of Exellence
+          BoBaas Academy of Excellence
         </p>
       </Link>
       <div className="flex-between gap-5">
